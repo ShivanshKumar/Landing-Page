4ac52cd1b7190435076d5fd922cf595f54580bdb
Shivansh <shivanshkumar693@gmail.com>
2020-03-13 20:34:50 +0530

    Updates

---
+++ precache-manifest.0bc5bdca78b967fe83f6c27e0dc26432.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d3ee7b60fe53ded16da1de2fc23c2a",
+    "url": "/Landing-Page/index.html"
+  },
+  {
+    "revision": "d886ec3d36f8ec0b0ed1",
+    "url": "/Landing-Page/static/css/main.3d62044e.chunk.css"
+  },
+  {
+    "revision": "140e21280a660cf2383b",
+    "url": "/Landing-Page/static/js/2.b722e611.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/Landing-Page/static/js/2.b722e611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d886ec3d36f8ec0b0ed1",
+    "url": "/Landing-Page/static/js/main.cd5b1071.chunk.js"
+  },
+  {
+    "revision": "f5f5cc68297b93129ba6",
+    "url": "/Landing-Page/static/js/runtime-main.4371b47b.js"
+  },
+  {
+    "revision": "83b61d3f45b60be43f3d2a536d95fc8e",
+    "url": "/Landing-Page/static/media/Custom.83b61d3f.png"
+  },
+  {
+    "revision": "1786632ef3b5997766eace51b459453c",
+    "url": "/Landing-Page/static/media/CustomHover.1786632e.png"
+  },
+  {
+    "revision": "15c38f512e23447c74aa27401e13a3d9",
+    "url": "/Landing-Page/static/media/Des.15c38f51.png"
+  },
+  {
+    "revision": "297bbc78b80f798b63bd3d602946162e",
+    "url": "/Landing-Page/static/media/Envato.297bbc78.png"
+  },
+  {
+    "revision": "6e7e5bf03d4b4ee3ad913c700e512721",
+    "url": "/Landing-Page/static/media/Gear.6e7e5bf0.png"
+  },
+  {
+    "revision": "25195f5faeb5c6c0255f6cd3f91c290a",
+    "url": "/Landing-Page/static/media/Girl.25195f5f.png"
+  },
+  {
+    "revision": "907f0c331b116d6abf846c57db2fa54e",
+    "url": "/Landing-Page/static/media/Guys.907f0c33.png"
+  },
+  {
+    "revision": "7ff31d5531082cbe2b4b136fa64f9d00",
+    "url": "/Landing-Page/static/media/Logo.7ff31d55.png"
+  },
+  {
+    "revision": "0acef94ee8e530387ea124736dcffa88",
+    "url": "/Landing-Page/static/media/LogoDark.0acef94e.png"
+  },
+  {
+    "revision": "90802a8124546e7b74734d50e9c7d4a2",
+    "url": "/Landing-Page/static/media/Man.90802a81.png"
+  },
+  {
+    "revision": "cf0d04180de1b17f2392a0eea00a1526",
+    "url": "/Landing-Page/static/media/Ok.cf0d0418.png"
+  },
+  {
+    "revision": "d1c40b81ad698b03e8072ca221454a4a",
+    "url": "/Landing-Page/static/media/Shield.d1c40b81.png"
+  },
+  {
+    "revision": "04c4a2572f34c2f80399d03d9572aba1",
+    "url": "/Landing-Page/static/media/Social.04c4a257.png"
+  }
+]);
